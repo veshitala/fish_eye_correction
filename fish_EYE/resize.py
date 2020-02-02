672875f2f4d5cd976afaf9cd93b318c3d0ad8ed6
sheetala prasad tiwari <veshitala@gmail.com>
2020-02-02 14:12:44 +0530

    All done
    uploaded file via git

---
+++ fish_EYE/resize.py
@@ -0,0 +1,15 @@
+from PIL import Image
+import os, sys
+
+path = "./photos/"
+dirs = os.listdir( path )
+
+def resize():
+    for item in dirs:
+        if os.path.isfile(path+item):
+            im = Image.open(path+item)
+            f, e = os.path.splitext(path+item)
+            imResize = im.resize((200,200), Image.ANTIALIAS)
+            imResize.save(f + ' resized.png', 'png', quality=90)
+
+resize()
